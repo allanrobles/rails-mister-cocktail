9f47e8dd3b4f812f1a463c165fd88cac3a8e8c0f
Allan Paladino <allanrp@gmail.com>
2020-10-20 22:46:35 -0300

    cocktail done up until cocktail routes/controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # A user can see the list of cocktails
+  root to: 'cocktails#index'
+  get 'cocktails', to: 'cocktails#index'
+
+  # A user can create a new cocktail
+  get 'cocktails/new', to: 'cocktails#new'
+  post 'cocktails/', to: 'cocktails#create'
+
+  # A user can see the details of a given cocktail, with the dose needed for each ingredient
+  get 'cocktails/:id', to: 'cocktails#show'
+
+
+  # GET "cocktails/new"
+  # POST "cocktails"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+json_file = URI.open(url).read
+parsed_file = JSON.parse(json_file)
+drinks_array = parsed_file["drinks"]
+
+10.times do |i|
+  Ingredient.create(name: drinks_array[i].values.join)
+end
