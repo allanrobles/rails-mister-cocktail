afedcb71830617f21073d6ae33f757262e952863
Allan Paladino <allanrp@gmail.com>
2020-10-21 22:04:20 -0300

    back end done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,19 +1,39 @@
 class CocktailsController < ApplicationController
 
+  before_action :set_cocktail, only: [:show]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
-    # fetch doses, ingredients
+    # before_action => set_cocktail => @cocktail = Cocktail.find(params[:id])
+
+    # TO DO: fetch doses, ingredients
   end
 
   def new
     @cocktail = Cocktail.new
+
   end
 
   def create
-    # do something
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,28 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # A user can see the list of cocktails
-  root to: 'cocktails#index'
-  get 'cocktails', to: 'cocktails#index'
+  # get 'cocktails',                  to: 'cocktails#index'
 
-  # A user can create a new cocktail
-  get 'cocktails/new', to: 'cocktails#new'
-  post 'cocktails/', to: 'cocktails#create'
+  # # A user can create a new cocktail
+  # get 'cocktails/new',              to: 'cocktails#new'
+  # post 'cocktails/',                to: 'cocktails#create'
 
   # A user can see the details of a given cocktail, with the dose needed for each ingredient
-  get 'cocktails/:id', to: 'cocktails#show'
+  # get 'cocktails/:id',              to: 'cocktails#show'
+
+  # A user can add a new dose (ingredient/description pair) to an existing cocktail
+  # get 'cocktails/:id/doses/new',    to: 'doses#new'
+  # post 'cocktails/:id/doses',       to: 'doses#create'
 
 
-  # GET "cocktails/new"
-  # POST "cocktails"
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+
+  root                              to: 'cocktails#index'
+  delete 'doses/:id',               to: 'doses#destroy', as: 'delete_dose'
+
+
+
 
 end
